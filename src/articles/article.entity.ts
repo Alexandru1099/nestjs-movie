8c220941a5600374ef43058a2aa7e78bb94da07a
Alex <cervinschi_alex@yahoo.com>
2022-05-04 19:13:05 +0300

    rez conflicte

--- src/articles/article.entity.ts
+++ src/articles/article.entity.ts
@@ -1,19 +1,26 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn } from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class Articles {
-    @PrimaryGeneratedColumn()
-    public id!: number;
+  @PrimaryGeneratedColumn()
+  public id!: number;
 
-    @Column({ type: "varchar" })
-    public name: string;
+  @Column({ type: 'varchar' })
+  public name: string;
 
-    @CreateDateColumn({ type: "timestamp" })
-    public createdAt!: Date;
+  @CreateDateColumn({ type: 'timestamp' })
+  public createdAt!: Date;
 
-    @UpdateDateColumn({ type: "timestamp" })
-    public updatedAt!: Date;
+  @UpdateDateColumn({ type: 'timestamp' })
+  public updatedAt!: Date;
 
-    @DeleteDateColumn({ type: "timestamp" })
-    public deleteAdt!: Date;
+  @DeleteDateColumn({ type: 'timestamp' })
+  public deleteAdt!: Date;
 }
